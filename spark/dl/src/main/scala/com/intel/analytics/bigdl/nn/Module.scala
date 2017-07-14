65f8210fef14c302fda26eed4b72e21bd210aab2
Wang Yanzhang <i8run15@gmail.com>
2017-07-14 12:13:43 +0800

    refactor: convolution refactor

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/Module.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/Module.scala
@@ -151,7 +151,7 @@ object Module {
             conv.padW,
             conv.padH,
             conv.nGroup)
-          quantizedConv.init(conv.weight, conv.bias)
+          quantizedConv.initWeightAndBias(conv.weight, conv.bias)
         case dilatedConv if dilatedConv.isInstanceOf[SpatialDilatedConvolution[T]] =>
           // do with dilated convolution
           dilatedConv
